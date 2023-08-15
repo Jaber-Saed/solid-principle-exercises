654d441496804aa91f0d99bdf18d45055f84af80
JaberSaed <jaber@capella.io>
2023-08-15 17:32:49 +0300

    add

---
+++ DIP_Console/DIP_Console/Model/EmployeeDataAccessLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP_Console.Model
+{
+    public interface IEmployeeDataAccessLogic
+    {
+        Employee GetEmployeeDetails(int id);
+    }
+    public class EmployeeDataAccessLogic: IEmployeeDataAccessLogic
+    {
+
+        public Employee GetEmployeeDetails(int id)
+        {
+ 
+            Employee emp = new Employee()
+            {
+                ID = id,
+                Name = "Pranaya",
+                Department = "IT",
+                Salary = 10000
+            };
+            return emp;
+        }
+    }
+  
+}
---
+++ LSP_Console solve/LSP_Console/Model/Fruit.cs
@@ -0,0 +1,23 @@
+﻿//ensure that you can substitute an instance of Orange for an instance of Apple without any problems
+
+namespace LSP.Model
+{
+    public abstract class Fruit
+    {
+        public abstract string GetColor();
+    }
+    public class Apple : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : Fruit
+    {
+        public override string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
